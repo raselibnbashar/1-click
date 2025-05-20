9cb64789368446a8cc01e9163b0db130bc1e5539
Rasel Ibn Bashar <63829981+raselibnbashar@users.noreply.github.com>
2025-05-20 17:55:35 +0600

    Delete linkedin-data-copy-extension directory

--- linkedin-data-copy-extension/content.js
+++
@@ -1,7 +0,0 @@
-function getData() {
-  const profileUrl = window.location.href;
-  const usernameMatch = profileUrl.match(/\/in\/(.*)\//);
-  const username = usernameMatch ? usernameMatch[1] : "";
-
-  return { profileUrl, username };
-}
